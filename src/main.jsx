664f37b2209ab1cb6c11450fc324f17fa682e120
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-11-14 19:44:58 -0300

    add - diretory structure and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './pages/LandingPage/index.jsx'
+import Authentication from './pages/Authentication/index.jsx'
+import Plaftorm from './pages/Platform/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <LandingPage />},
+      {path: '/auth', element: <Authentication/>},
+      {path: '/home', element: <Plaftorm/>},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
