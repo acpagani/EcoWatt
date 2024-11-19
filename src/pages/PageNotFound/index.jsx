09469a0ab38578c3dda06c3b0ec87c14f5ddbb5a
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-11-18 21:54:37 -0300

    feat - PageNotFound

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import Header from "../LandingPage/components/Header";
+
 export default function PageNotFound() {
   return (
     <>
-    
+      <Header/>
+      <main className="h-[90vh] w-screen flex items-center justify-center">
+        <section className="flex flex-col gap-4 items-center justify-center">
+          <h1 className="text-9xl text-center font-bold">404</h1>
+          <p className="text-center text-5xl">Ops! Parece que você se perdeu...</p>
+          <Link to={"/"} className="bg-eDarkBlue px-5 py-3 rounded-lg font-bold text-eWhite text-3xl hover:bg-eGreen hover:scale-110 transition-all duration-300">Voltar para página de início</Link>
+        </section>
+      </main>
     </>
   )
 }
