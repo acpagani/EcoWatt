ad4c80315d3a45ac3bb7c53cbd8f96c0c81cdd78
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-11-19 00:01:07 -0300

    feat: hero section responsive

--- src/pages/LandingPage/Sections/Hero/index.jsx
+++ src/pages/LandingPage/Sections/Hero/index.jsx
@@ -10,28 +10,31 @@ const Hero = forwardRef(({ refNext }, ref) => {
   return (
     <section
       ref={ref}
-      className="w-full h-[90vh] bg-repeat"
+      className="w-full h-[90vh] bg-repeat max-md:h-auto"
       style={{
         backgroundImage:
           "url('https://www.transparenttextures.com/patterns/cubes.png')",
       }}
     >
-      <div className="container mx-auto px-20 flex flex-col h-full justify-between pb-10">
-        <div className="flex items-center flex-1 justify-evenly container">
-          <div className="flex flex-col justify-center items-start">
-            <h1 className="text-7xl font-bold text-eGray">EcoWatt</h1>
-            <p className="text-xl text-eGray">
+      <div className="container mx-auto px-20 flex flex-col h-full justify-between pb-10 max-md:w-full max-md:px-10 max-md:py-10">
+        <div className="flex items-center flex-1 justify-evenly container max-md:flex-col max-md:gap-5">
+          <div className="flex flex-col justify-center items-start ">
+            <h1 className="text-7xl font-bold text-eGray max-md:text-center max-sm:text-6xl">EcoWatt</h1>
+            <p className="text-xl text-eGray max-md:text-center">
               O futuro da energia nas indústrias
             </p>
-            <Link to={"/auth"} className="bg-eBlue text-eGray px-6 py-2 mt-4 border border-black rounded-lg hover:bg-eDarkBlue hover:text-eWhite transition-colors duration-300">
+            <Link to={"/auth"} className="bg-eBlue text-eGray px-6 py-2 mt-4 border border-black rounded-lg hover:bg-eDarkBlue hover:text-eWhite transition-colors duration-300 max-md:hidden">
               Comece agora
             </Link>
           </div>
-          <video src="/assets/video/heroVideo.mp4" autoPlay muted width={"300px"} height={"400px"} loop style={{
+          <video src="/assets/video/heroVideo.mp4" className="max-md:w-3/4" autoPlay muted width={"300px"} height={"400px"} style={{
             borderRadius: "20px",
           }}>
 
           </video>
+          <Link to={"/auth"} className="hidden max-md:flex w-3/4 justify-center items-center bg-eBlue text-eGray px-6 py-2 mt-4 border border-black rounded-lg hover:bg-eDarkBlue hover:text-eWhite transition-colors duration-300">
+              Comece agora
+            </Link>
         </div>
         <button onClick={() => goToSection(refNext)}>
           <IoIosArrowDown className="text-sWhite text-4xl mx-auto mt-10 animate-bounce" />
